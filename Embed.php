abff352e40e8c615817c0c9fab86b0d7153b0bde
Alexander Mashin <alex-mashin@users.noreply.github.com>
2023-10-12 09:14:49 +0700

    Language
    Language

--- Embed.php
+++ Embed.php
@@ -159,7 +159,7 @@ class Embed {
 	public static function setup( Parser &$parser ): bool {
 		// Add all domains to CSP header:
 		$csp_domains = [];
-		$csp_explicit =  wfMessage( 'embed-csp' )->text();
+		$csp_explicit =  wfMessage( 'embed-csp', [], 'en' )->plain();
 		if ( $csp_explicit ) {
 			$csp_domains = explode( ',', $csp_explicit );
 		} else {
